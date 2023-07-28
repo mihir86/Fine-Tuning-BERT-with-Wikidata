4bc466900d96f62fd6652b3ac86218b9591ead81
Mihir Bansal <mstrmihir2@gmail.com>
2023-07-28 15:40:36 +0530

    Add files via upload

---
+++ Dataset/Preprocessing Data/furniture/dataset_for_probing.py
@@ -0,0 +1,23 @@
+import sys
+import csv
+import pandas as pd
+import numpy as np
+import io
+
+#cd "BPHC Internships\3.University of Hamburg"
+
+for i in range(1,4):
+    df = pd.read_csv(f'List_{i}.csv')
+    print(df)
+
+    f= io.open(f'{i}.masked_data_width.txt', "w+", encoding="utf-8")
+
+    for l in range(df.shape[0]):
+        for m in range(df.shape[0]):    
+            if(l != m):
+                obj1 = df['itemLabel'][l]
+                obj2 = df['itemLabel'][m]
+                #print("Which is wider, and thus has more [MASK], ", obj1, " or ", obj2)
+                f.write("Which is wider, and thus has more [MASK], " + obj1 + " or " + obj2 + "\n")
+                
+    f.close()
